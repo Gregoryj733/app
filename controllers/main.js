3e50e1be0397465a42ad0308f500cf7b56a2db42
Gregory Rivera <Gregoryj733@gmail.com>
2018-08-04 13:36:14 -0400

    primera app

---
+++ controllers/main.js
@@ -0,0 +1,9 @@
+exports.home=(req,res)=>{
+    res.send("Bienvenidos a Mi Página");
+    res.end();
+}
+
+exports.contacto=(req,res)=>{
+    res.send("Puedes contactarme al correo gregoryj733@gmail.com");
+    res.end();
+}
